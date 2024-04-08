6547ee5951ba968c29f9815a9717e125a8cbd181
yi.luo <lyyl6270@163.com>
2024-04-08 11:22:45 +0800

    feat:组合式api使用

---
+++ src/views/baseView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="page">
+    <h2>组合式api学习</h2>
+    <div>{{ user }}</div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref, watch } from "vue";
+
+// const phone = ref("180");
+
+const user = reactive({
+  username: "yyds",
+  age: 20,
+});
+
+// 监听整个对象
+watch(
+  () => {
+    return {
+      ...user,
+    };
+  },
+  (newVal, oldVal) => {
+    console.log(newVal, oldVal);
+  }
+  //   { deep: true }
+);
+user.username = "111";
+
+// 监听单个属性
+
+// watch(
+//   () => user.age,
+//   (newVal, oldVal) => {
+//     console.log(newVal, oldVal);
+//   }
+// );
+// user.age = 18;
+</script>
+
+<style lang="scss">
+.page {
+}
+</style>
---
+++ src/views/gradeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page">
+    <h2>组合式api学习</h2>
+    <div>
+      {{ ageRef }}
+    </div>
+    <div>
+      {{ stateToRefs }}
+    </div>
+
+    <button @click="handleClickAge">增加</button>
+  </div>
+</template>
+
+<script setup>
+import { reactive, toRef, toRefs } from "vue";
+
+const user = reactive({
+  name: "didi",
+  age: 18,
+});
+
+// toRef 基于对象上的一个属性创建出来的一个ref
+const ageRef = toRef(user, "age");
+console.log("==>Get ageRef", ageRef);
+
+// toRefs
+const state = reactive({
+  foo: 1,
+  bar: 2,
+});
+
+const stateToRefs = toRefs(state);
+console.log(state.foo, stateToRefs.foo.value);
+
+const handleClickAge = () => {
+  ageRef.value++;
+  //   state.foo++;
+  stateToRefs.foo.value++;
+};
+</script>
+
+<style lang="scss"></style>
